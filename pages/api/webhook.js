50594d1e1754c03b88d82deed53131f8347c7dec
Designrpros <Designrpros@.github.com>
2024-03-05 18:02:25 +0100

    environment virables

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,14 +2,25 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // Assuming you've initialized the Firebase Admin SDK correctly (globally or within this file)
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-service-account.json');
 
-// Initialize Firebase Admin SDK if not already initialized
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+            type: process.env.FIREBASE_TYPE,
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+            // Note: private_key needs to have actual line breaks replaced with \n
+            private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            client_id: process.env.FIREBASE_CLIENT_ID,
+            auth_uri: process.env.FIREBASE_AUTH_URI,
+            token_uri: process.env.FIREBASE_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+        }),
     });
 }
+
 
 const db = admin.firestore();
 
